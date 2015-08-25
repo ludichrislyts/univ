07b8b312ab9bee42170880133063534b27478a64
jose torres & logan wu <student@epicodus.com>
2015-08-25 10:35:42 -0700

    add course.php

---
+++ src/Course.php
@@ -0,0 +1,85 @@
+<?php
+    class Course
+    {
+        private $course_name;
+        private $course_number;
+        private $id;
+
+        function __construct($course_name, $course_number, $id = null)
+        {
+            $this->course_name = $course_name;
+            $this->course_number = $course_number;
+            $this->id - $id;
+        }
+
+        function setCourseName($new_course_name)
+        {
+            $this->course_name = (string) $new_course_name;
+        }
+
+        function getCourseName()
+        {
+            return $this->course_name;
+        }
+
+        function setCourseNumber()($new_course_number)
+        {
+            $this->course_number = (string) $new_course_number;
+        }
+
+        function getCourseNumber()
+        {
+            return $this->course_number;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO courses (course_name, course_number) VALUES ('{$this->getCourseName()}', '{$this->getCourseNumber()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_courses = $GLOBALS['DB']->query("SELECT * FROM courses;");
+            $courses = array();
+            foreach($returned_courses as $course) {
+                $course_name = $course['course_name'];
+                $course_number = $course['course_number'];
+                $id = $course['id'];
+                $new_course = new Course($course_name, $course_number, $id);
+                array_push($courses, $new_course);
+            }
+            return $courses;
+        }
+
+        static function find($search_id)
+        {
+            $found_course = null;
+            $courses = Course::getAll()
+            foreach($courses as $course) {
+                $course_id = $course->getId();
+                if ($course_id == $search_id) {
+                    $found_course = $course;
+                }
+            }
+            return $found_course;
+        }
+
+        function update($new_course_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE courses SET course_name = '{$new_course_name}' WHERE id = {$this->getId()};");
+            $this->setCourseName($new_course_name);
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM courses WHERE id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM courses_students WHERE course_id = {$this->getId()};");
+        }
+    }
+ ?>
